e2b8b3d89acf11e21bca5fb2183c7019d06b3473
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2022-12-05 22:04:29 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const convertBtn = document.getElementById("convert-btn")
+const inputEl = document.getElementById("input").value
+const lengthEl = document.getElementById("length-covert")
+const volumeEl = document.getElementById("volume-covert")
+const massEl = document.getElementById("mass-covert")
+
+let lengthStr =""
+let volumeStr =""
+let massStr =""
+convertBtn.addEventListener("click", function(){
+    
+})
